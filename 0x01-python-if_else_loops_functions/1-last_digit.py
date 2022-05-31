60936412e23b0a13e65d593f14dbef373b643013
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-05-31 23:06:42 +0300

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdig = number % 10
+strg1 = "Last digit of "
+strg2 = " is "
+greater = " and is greater than 5"
+zero = " and is 0"
+less = " and is less than 6 and not 0"
+
 if number >= 0:
-    if lastdig > 5:
-        print("Last digit of {} is {} and\
- is greater than 5".format(number, lastdig))
-    elif lastdig == 0:
-        print("Last digit of {} is {} and\
- is 0".format(number, lastdig))
-    else:
-        print("Last digit of {} is {} and\
- is less than 6 and not 0".format(number, lastdig))
+    ldigit = number % 10
+if number < 0:
+    ldigit = number % -10
+
+if ldigit > 5:
+    print("{}{:d}{}{:d}{}".format(strg1, number, strg2, ldigit, greater))
+elif ldigit == 0:
+    print("{}{:d}{}{:d}{}".format(strg1, number, strg2, ldigit, zero))
 else:
-    lastdigneg = -number  # el modulo de un numero negativo es diferente
-    if lastdigneg % 10 == 0:
-        print("Last digit of {} is {} and\
- is 0".format(number, -(lastdigneg % 10)))
-    else:
-        print("Last digit of {} is {} and\
- is less than 6 and not 0".format(number, -(lastdigneg % 10)))
+    print("{}{:d}{}{:d}{}".format(strg1, number, strg2, ldigit, less))
 
