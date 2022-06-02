7dd56441b467560cd0e4f40f698fb85c1835549b
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-06-02 21:16:18 +0300

    commit to 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    for i in range(0, len(name)):
-        if name[i][0] != "_" and name[i][1] != "_":
-            print("{}".format(name[i]))
+    import sys
+    index = 0
+    listed = (dir(hidden_4))
+    listlen = len(listed)
+    for elements in listed:
+        if elements[0] == '_':
+            continue
+        else:
+            print(elements)
