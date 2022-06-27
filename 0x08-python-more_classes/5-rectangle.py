a3307956e90c955703a447cc924873bfe7b4469a
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-06-27 20:55:59 +0300

    commit to 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,73 +1,79 @@
 #!/usr/bin/python3
-"""Module of real definition of a rectangle"""
+"""
+Module for task 5
+"""
 
 
-class Rectangle():
-    """class Rectangle defined with priv inst attrib width and height"""
+class Rectangle:
+    """Defines a Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """method to get value of width"""
-        return self.__width
+        """Instantiation attributes"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
 
     @property
     def height(self):
-        """method to get value of height"""
+        """Property to retrieve height"""
         return self.__height
-
-    @width.setter
-    def width(self, value):
-        """method to set the value of width"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
 
     @height.setter
     def height(self, value):
-        """method to set the value of height"""
-        if type(value) != int:
+        """Setter to set height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
+
+    @property
+    def width(self):
+        """Property to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter to set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     def area(self):
-        """publ ins method to determine area of rect"""
-        ar = self.__width * self.__height
-        return ar
+        """Calculates the area of a Rectangle"""
+        return self.__width * self.__height
 
     def perimeter(self):
-        """publ inst method to determine perimeter of rect"""
-        per = 2 * (self.__width + self.__height)
+        """Calculates the permiter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
-            per = 0
-        return per
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """built in to return printed representation of string instance"""
-        fig = ""
-        if self.__height == 0 or self.__width == 0:
-                    return fig
-        for i in range(self.__height):
-            for j in range(self.__width):
-                fig += "#"
-            fig += '\n'
-        return fig[:-1]
+        """Prints the rectangle using #"""
+        sq = ""
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    sq += "#"
+                if i < self.__height - 1:
+                    sq += "\n"
+        return sq
 
     def __repr__(self):
-        """Built in method to return string representation of rect"""
-        return "Rectangle({}, {})".format(eval(repr(self.__width)), (
-                eval(repr(self.__height))))
+        """Return the rectangle"""
+        return 'Rectangle({}, {}'.format(self.__width, self.__height)
 
     def __del__(self):
-        """Builtin to ensure proper deletion of instance"""
+        """Print message when instance deleted"""
         print("Bye rectangle...")
-
