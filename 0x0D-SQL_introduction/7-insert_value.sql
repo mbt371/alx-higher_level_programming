4e40ec5e65547a1a1dabd6ccc2ef0af38c037b31
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-08-10 01:07:47 +0300

    commit to 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- insert a new row
--- in the table first_table
-INSERT INTO first_table (id, name) VALUES ('89', 'ALX School');
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "ALX School");
