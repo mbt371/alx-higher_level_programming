50f6febfdff59b7ad031f51ee047ba2bbd162fbc
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-09-23 21:53:11 +0300

    commit to 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
+""" script that fetches https://intranet.hbtn.io/status
+    -You must use the package requests """
 import requests
 
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    req = "Body response:\n\t- type: {}\n\t- content: {}"
-    print(req.format(type(response.text), response.text))
+
+if __name__ == '__main__':
+    url = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
