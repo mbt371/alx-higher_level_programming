2230e0101fec4d60aaff9ce2ca6f5d572befaf78
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-09-23 21:42:40 +0300

    commit to 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Error code with request """
+"""Response header value"""
+import requests
+from requests.exceptions import HTTPError
+import sys
 
 if __name__ == "__main__":
-    import requests
-    import sys
     req = requests.get(sys.argv[1])
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
